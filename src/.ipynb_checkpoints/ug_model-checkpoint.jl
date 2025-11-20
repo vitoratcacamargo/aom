e0dc5e50e87861c13853c0785dddc7970464ae30
Vitor Alexandre Theodosio Carvalho A Camargo <psyc2610_vt245@login1.bouchet.ycrc.yale.edu>
2025-11-20 11:21:28 -0500

    Created UGmodel module to centralize lexicon

---
+++ src/.ipynb_checkpoints/ug_model-checkpoint.jl
@@ -0,0 +1,11 @@
+module UGmodel
+
+const LEXICON = (
+    nouns = ["dog", "cat", "fox"],
+    verbs_trans = ["see", "chase", "attack"],
+    verbs_intr = ["sleep", "run"],
+    adjectives = ["big", "small"],
+    wh_word = "what",
+)
+    
+end
